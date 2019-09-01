e029d84e31acdf154feacde58cafbe63bedb64b9
michael1011 <me@michael1011.at>
2019-09-01 12:47:58 +0200

    feat: style input

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -1,4 +1,5 @@
-import { Input } from '@material-ui/core';
+import styles from '../css/styles.css';
+import { TextField } from '@material-ui/core';
 import React, { FunctionComponent } from 'react';
 
 type Props = {
@@ -7,8 +8,8 @@ type Props = {
 
 const TextInput: FunctionComponent<Props> = ({ onChange }) => {
   return (
-    <div>
-      <Input onChange={(event) => {
+    <div id={styles.valueInput}>
+      <TextField label={"Value"} fullWidth={true} autoFocus={true} multiline={true} onChange={(event) => {
         onChange(event.target.value);
       }}/>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
+import Head from 'next/head';
 import styles from '../css/styles.css';
 import React, { Component } from 'react';
-import DownloadableQRCode from '../components/DownloadableQRCode';
 import TextInput from '../components/TextInput';
+import DownloadableQRCode from '../components/DownloadableQRCode';
 
 type State = {
   qrValue: string;
@@ -11,15 +12,22 @@ class Index extends Component<{}, State> {
   render() {
     return (
       <div>
-        <h1 id={styles.title}>Generate QR codes</h1>
+        <Head>
+          <title>QR-Code generator</title>
+        </Head>
+
+        <h1 id={styles.title}>QR-Code generator</h1>
 
         <div className={styles.wrapper}>
           <DownloadableQRCode value={this.state !== null ? this.state.qrValue : ''} />
-          <TextInput onChange={(newText) => {
-            this.setState({
-              qrValue: newText,
-            });
-          }} />
+
+          <div className={styles.config}>
+            <TextInput onChange={(newText) => {
+              this.setState({
+                qrValue: newText,
+              });
+            }} />
+          </div>
         </div>
       </div>
     );
